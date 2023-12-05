163303a0c53de8c9f6a5a6106b41c82fd776fd1e
Ebarringer2 <149868930+Ebarringer2@users.noreply.github.com>
2023-12-05 11:00:37 -0800

    Update sprites.py
    switching format to single class for player management

--- sprites.py
+++ sprites.py
@@ -1,66 +1,276 @@
+# This file was created by Elliott Barringer
+
+# import necessary libs and modules
 import pygame as pg
 from pygame.sprite import Sprite
-
 from pygame.math import Vector2 as vec
 import os
 from settings import *
+from threading import Timer
+from random import randint as r
 
-# setup asset folders here - images sounds etc.
+# set up for asset folders
+
 game_folder = os.path.dirname(__file__)
 img_folder = game_folder
-snd_folder = game_folder
 
-# class for player A
-class Player_A(Sprite):
-    def __init__(self, game):
+# Set up for Player using WASD
+class Player(Sprite):
+
+    def __init__(self, x, y, w, h, k_left, k_right, k_jump, game):
+
         Sprite.__init__(self)
-        # self.image = pg.Surface((50, 50))
-        # self.image.fill(GREEN)
-        # use an image for player sprite...
-        self.game = game
-        self.image = pg.image.load(os.path.join(img_folder, '')).convert()
-        self.image.set_colorkey(BLACK)
+
+        self.image = pg.Surface((w, h))
+        self.image.fill(RED)
         self.rect = self.image.get_rect()
-        self.rect.center = (0, 0)
-        self.pos = vec(WIDTH/2, HEIGHT/2)
-        self.vel = vec(0,0)
-        self.acc = vec(0,0) 
+        self.rect.x = x
+        self.rect.y = y
+        self.pos = vec(x, y)
+        self.game = game
+        self.k_left = k_left
+        self.k_right = k_right
+        self.k_jump = k_jump
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.speed = PLAYER_SPEED
+        self.hop = PLAYER_JUMP
+
+        '''self.image = pg.Surface((20, 20))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.game = game
+        # self.image = pg.Surface((20, 20))
+        # self.image.fill(WHITE)
+        # self.rect = self.image.get_rect()
+        # self.rect.center = (0, 0)
+        self.rect.x
+        self.pos = vec(x, y)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.speed = PLAYER_SPEED
+        self.hop = PLAYER_JUMP
+        print("Spawned")'''
+
     def controls(self):
+
         keys = pg.key.get_pressed()
-        if keys[pg.K_a]:
-            self.acc.x = -5
-        if keys[pg.K_d]:
-            self.acc.x = 5
-        if keys[pg.K_SPACE]:
+
+        if keys[self.k_left]:
+
+            self.acc.x = -self.speed
+        
+        if keys[self.k_right]:
+
+            self.acc.x = self.speed
+        
+        if keys[self.k_jump]:
+
             self.jump()
+    
     def jump(self):
+
         p_hits = pg.sprite.spritecollide(self, self.game.all_platforms, False)
+
         if p_hits:
-            print("i can jump")
-            self.vel.y = -PLAYER_JUMP
+
+            self.vel.y = - PLAYER_JUMP
 
     def update(self):
-        self.acc = vec(0,PLAYER_GRAV)
+
+        self.acc = vec(0, PLAYER_GRAV)
         self.controls()
-        # if friction - apply here
+
         self.acc.x += self.vel.x * -PLAYER_FRIC
-        # self.acc.y += self.vel.y * -0.3
-        # equations of motion
+
+        # motion eqs
+
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
         self.rect.midbottom = self.pos
 
-# class for player B
+        # check for interaction with powerups
 
-class Player_B(Sprite):
+        speed = pg.sprite.spritecollide(self, self.game.all_speed, True)
+        super_jump = pg.sprite.spritecollide(self, self.game.all_super_jump, True)
+        size_mod = pg.sprite.spritecollide(self, self.game.all_size_mod, True)
 
+        t = Timer(self.reset(), 5)
+
+        self.check_powerup(speed, super_jump, size_mod, t)
+
+    def check_powerup(self, cond1, cond2, cond3, timer):
+
+        if cond1:
+
+            self.speed += 5
+            timer.start()
+        
+        elif cond2:
+
+            self.hop += 30
+            timer.start()
+        
+        elif cond3:
+
+            self.image = pg.Surface((50, 50))
+            timer.start()
+        
+        else:
+
+            pass
+
+    def reset(self):
+
+        self.speed = PLAYER_SPEED
+        self.hop = PLAYER_JUMP
+        self.image = pg.Surface((20, 20))        
+
+# class for player using arrow keys
+'''class PlayerB(Sprite):
+
+    def __init__(self, game):
+
+        Sprite.__init__(self)
+        
+        self.game = game
+        self.image = pg.Surface((20, 20))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        #self.rect.center = (0, 0)
+        self.pos = vec(WIDTH/2 + 80, HEIGHT/2)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.speed = PLAYER_SPEED
+        self.hop = PLAYER_JUMP
+
+    def controls(self):
+# check for plau controls 
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_LEFT]:
+
+            self.acc.x = -self.speed
+        
+        if keys[pg.K_RIGHT]:
+
+            self.acc.x = self.speed
+        
+        if keys[pg.K_UP]:
+
+            self.jump()
     
+    def jump(self):
 
+        p_hits = pg.sprite.spritecollide(self, self.game.all_platforms, False)
 
-# platforms
+        if p_hits:
 
+            self.vel.y = - PLAYER_JUMP
+
+    def update(self):
+
+        self.acc = vec(0, PLAYER_GRAV)
+        self.controls()
+
+        self.acc.x += self.vel.x * -PLAYER_FRIC
+
+        # motion eqs
+
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.midbottom = self.pos
+
+        # check for interaction with powerups
+
+        speed = pg.sprite.spritecollide(self, self.game.all_speed, True)
+        super_jump = pg.sprite.spritecollide(self, self.game.all_super_jump, True)
+        size_mod = pg.sprite.spritecollide(self, self.game.all_size_mod, True)
+
+        t = Timer(self.reset(), 5)
+
+        self.check_powerup(speed, super_jump, size_mod, t)
+
+    def check_powerup(self, cond1, cond2, cond3, timer):
+
+        if cond1:
+
+            self.speed += 5
+            timer.start()
+        
+        elif cond2:
+
+            self.hop += 30
+            timer.start()
+        
+        elif cond3:
+
+            self.image = pg.Surface((50, 50))
+            timer.start()
+        
+        else:
+
+            pass
+
+    def reset(self):
+
+        self.speed = PLAYER_SPEED
+        self.hop = PLAYER_JUMP
+        self.image = pg.Surface((20, 20))      
+'''
+# class for powerups
+class Powerup(Sprite):
+
+    def __init__(self, x, y, w, h, kind, game):
+
+        Sprite.__init__(self)
+
+        self.image = pg.Surface((w, h))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.pos = vec(x, y)
+        self.kind = p_dict[str(kind)]
+        self.game = game
+
+        # sort powerups into sprite groups
+        all_super_jump = pg.sprite.Group()
+        all_super_speed = pg.sprite.Group()
+        all_size_mod = pg.sprite.Group()
+
+        if self.kind == "speed":
+            
+            all_super_speed.add(self)
+        
+        elif self.kind == "super_jump":
+
+            all_super_jump.add(self)
+        
+        elif self.kind == "size_mod":
+
+            all_size_mod.add(self)
+    
+    def update(self):
+        
+        c = pg.sprite.spritecollide(self, self.game.all_players, True)
+
+        t = Timer(self.reset(), 15)
+
+        if c:
+            # 'hide' the image by moving it off the screen
+            self.pos = vec(2 * WIDTH, 2 * HEIGHT)
+            t.start()
+    
+    def reset(self):
+
+        self.pos = vec(self.rect.x, self.rect.y)
+
+# class for platforms
 class Platform(Sprite):
+
     def __init__(self, x, y, w, h, category):
+
         Sprite.__init__(self)
         self.image = pg.Surface((w, h))
         self.image.fill(GREEN)
@@ -69,25 +279,17 @@ class Platform(Sprite):
         self.rect.y = y
         self.category = category
         self.speed = 0
+
         if self.category == "moving":
+
             self.speed = 5
+    
     def update(self):
+
         if self.category == "moving":
+
             self.rect.x += self.speed
+
             if self.rect.x + self.rect.w > WIDTH or self.rect.x < 0:
+
                 self.speed = -self.speed
-
-class Mob(Sprite):
-    def __init__(self, x, y, w, h, kind):
-        Sprite.__init__(self)
-        self.image = pg.Surface((w, h))
-        self.image.fill(RED)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.kind = kind
-        self.pos = vec(WIDTH/2, HEIGHT/2)
-
-    def update(self):
-        pass
-        
